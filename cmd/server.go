7887db33859fe8af6e7cffe3e85d29417108a975
thallium <tuogengchen@gmail.com>
2021-09-19 20:11:13 -0600

    start working on server

---
+++ cmd/server.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+type data struct {
+	Content string
+	Website string
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var d data
+	
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println(r.Body)
+	fmt.Println(d.Content)
+}
+
+// cfCmd represents the cf command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "start a server for parsing data from the broswer extension",
+	Long: "",
+	Run: func(cmd *cobra.Command, args []string) {
+		http.HandleFunc("/", handler)
+		http.ListenAndServe(":8090", nil);
+	},
+}
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cfCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cfCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/oriser/regroup v0.0.0-20201024192559-010c434ff8f3
 	github.com/spf13/cobra v1.1.3
-	github.com/spf13/viper v1.7.1
+	github.com/spf13/viper v1.9.0
+	golang.org/x/text v0.3.7 // indirect
 )
